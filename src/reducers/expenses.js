f80343c856f87aa84e95c70b57be8f065a29529e
Andries Niemandt <andries.niemandt91@gmail.com>
2019-02-09 23:22:00 +0200

    removing of data added

--- src/reducers/expenses.js
+++ src/reducers/expenses.js
@@ -24,8 +24,10 @@ export default (state = expensesReducerDefaultState, action) => {
                     return expense;
                 }
             });
+
         case 'SET_EXPENSES':
             return action.expenses;
+
         default:
             return state;
     }
